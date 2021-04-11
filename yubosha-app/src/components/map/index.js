fef342b04db86546827edf53fcd2de57c82233c3
Kurumi Kawamura <kurumikawamura@gmail.com>
2021-04-11 15:49:57 -0400

    favicon

--- yubosha-app/src/components/map/index.js
+++ yubosha-app/src/components/map/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { world_map } from "./worldmap";
 import { japan_map } from "./japanMap";
 import {
@@ -13,43 +13,66 @@ import {
 } from "@syncfusion/ej2-react-maps";
 import Header from "../header/index";
 import styled from "styled-components";
-
+let sample = [
+  {
+    latitude: 34.6937,
+    longitude: 135.5023,
+    location: "Osaka",
+    name: "Moss",
+    color: "skyblue",
+  },
+  {
+    latitude: 35.0116,
+    longitude: 135.7681,
+    location: "Kyoto",
+    name: "Moss2",
+    color: "skyblue",
+  },
+];
 const Map = () => {
+  const [src, setSrc] = useState(null);
+
+  useEffect(() => {
+    let arr = [];
+    sample.forEach((elem) => {
+      arr.push(elem);
+    });
+
+    setSrc(arr);
+  }, [setSrc]);
+  console.log(src);
+
   return (
     <>
       <Header />
-      
+
       <Wrapper>
         <H1>Moss Map</H1>
-        <MapsComponent id="container" >
-          <Inject services={[Marker, MapsTooltip]} />
-          <LayersDirective>
-            <LayerDirective
-              shapeData={japan_map}
-              shapeSettings={{ fill: "#4c7d62" }}
-            >
-              <MarkersDirective>
-                <MarkerDirective
-                  visible={true}
-                  tooltipSettings={{ visible: true, valuePath: "name" }}
-                  shape={"Balloon"}
-                  colorValuePath={"color"}
-                  width={17}
-                  height={17}
-                  dataSource={[
-                    {
-                      latitude: 34.6937,
-                      longitude: 135.5023,
-                      location: "Osaka",
-                      name: "Moss",
-                      color: "pink",
-                    },
-                  ]}
-                ></MarkerDirective>
-              </MarkersDirective>
-            </LayerDirective>
-          </LayersDirective>
-        </MapsComponent>
+        {src ? (
+          <MapsComponent id="container">
+            <Inject services={[Marker, MapsTooltip]} />
+            <LayersDirective>
+              <LayerDirective
+                shapeData={japan_map}
+                shapeSettings={{ fill: "#4c7d62" }}
+              >
+                <MarkersDirective>
+                  <MarkerDirective
+                    visible={true}
+                    tooltipSettings={{ visible: true, valuePath: "name" }}
+                    shape={"Balloon"}
+                    colorValuePath={"color"}
+                    width={13}
+                    height={13}
+                    dataSource={src}
+                  ></MarkerDirective>
+                </MarkersDirective>
+              </LayerDirective>
+            </LayersDirective>
+          </MapsComponent>
+        ) : (
+          <div>Loading...</div>
+        )}
       </Wrapper>
     </>
   );
@@ -64,7 +87,7 @@ const Wrapper = styled.div`
 `;
 
 const H1 = styled.h1`
-margin-bottom: 40px;
+  margin-bottom: 40px;
 `;
 
 export default Map;

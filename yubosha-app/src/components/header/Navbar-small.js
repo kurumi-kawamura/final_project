e400465e798eb7ee7d19a5b5b660a2093095b7a0
Kurumi Kawamura <kurumikawamura@gmail.com>
2021-04-20 12:01:02 -0400

    navbar

--- yubosha-app/src/components/header/Navbar-small.js
+++ yubosha-app/src/components/header/Navbar-small.js
@@ -3,9 +3,25 @@ import { CgMenuCake } from "react-icons/cg";
 import styled from "styled-components";
 
 const NavbarSmall = () => {
-  return <>
-  <CgMenuCake />
-  </>;
+  return (
+    <>
+      <Wrapper>
+        <CgMenuCake
+          style={{
+            width: "40px",
+            height: "40px",
+            color: "var(--soft-gray)",
+            cursor: "pointer",
+          }}
+        />
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 3px;
+`;
 
 export default NavbarSmall;
